313c34ceb405ff934701fc0b665e3c34cdbdceab
sangho527 <sy980527@naver.com>
2022-11-29 17:22:56 +0900

    [ADD] UserController 추가, UserJoinRequest를 통해 이름,패스워드,이메일 요청 221129

---
+++ src/main/java/controller/UserController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import domain.Response;
+import domain.dto.UserDto;
+import domain.dto.UserJoinRequest;
+import domain.dto.UserJoinResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public Response<UserJoinResponse> join(@RequestBody UserJoinRequest userJoinRequest) {
+        UserDto userDto = userService.join(userJoinRequest);
+        return Response.success(new UserJoinResponse());
+    }
+}
