50c2595853a101dd480c839cfd79144f70e28bf2
sangho527 <sy980527@naver.com>
2022-11-29 17:23:46 +0900

    [ADD] UserJoinResponse 추가, 응답은 이름과 이메일만 221129

---
+++ src/main/java/domain/dto/UserJoinResponse.java
@@ -0,0 +1,13 @@
+package domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserJoinResponse {
+    private String userName;
+    private String email;
+}
