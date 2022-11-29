e674b3f651be8971e08f68212aa22bd21360a884
sangho527 <sy980527@naver.com>
2022-11-29 22:44:58 +0900

    [UPDATE] User 수정, unique 제약조건 및 변수명 수정 221129

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -2,19 +2,24 @@ package domain;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Entity
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true) // 단일 필드에서 사용, 유일한 값만 들어가도록 하는 제약조건
     private String userName;
     private String password;
-    private String emailAddress;
+    private String email;
 }
